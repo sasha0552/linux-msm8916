03a16c8faf2854ab92ff3f20be68f15727fe2f20
sasha0552 <admin@sasha0552.org>
2023-08-23 08:49:37 +0000

    temp commit

---
+++ drivers/input/touchscreen/cyttsp4_devtree.h
@@ -0,0 +1,21 @@
+#ifndef _LINUX_CYTTSP4_DEVTREE_H
+#define _LINUX_CYTTSP4_DEVTREE_H
+
+#include <linux/device.h>
+
+#ifdef CONFIG_TOUCHSCREEN_CYTTSP4_DEVTREE
+int cyttsp4_devtree_create_and_get_pdata(struct device *adap_dev);
+int cyttsp4_devtree_clean_pdata(struct device *adap_dev);
+#else
+static inline int cyttsp4_devtree_create_and_get_pdata(struct device *adap_dev)
+{
+	return 0;
+}
+
+static inline int cyttsp4_devtree_clean_pdata(struct device *adap_dev)
+{
+	return 0;
+}
+#endif
+
+#endif /* _LINUX_CYTTSP4_DEVTREE_H */
