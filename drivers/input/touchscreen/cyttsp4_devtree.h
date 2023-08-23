5ee75d2cd3eb29f5810c838e4cafc773fa81a981
sasha0552 <admin@sasha0552.org>
2023-08-23 08:55:17 +0000

    force enable devtree

--- drivers/input/touchscreen/cyttsp4_devtree.h
+++ drivers/input/touchscreen/cyttsp4_devtree.h
@@ -3,7 +3,7 @@
 
 #include <linux/device.h>
 
-#ifdef CONFIG_TOUCHSCREEN_CYTTSP4_DEVTREE
+#if 1
 int cyttsp4_devtree_create_and_get_pdata(struct device *adap_dev);
 int cyttsp4_devtree_clean_pdata(struct device *adap_dev);
 #else
